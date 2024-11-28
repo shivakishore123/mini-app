67fce81045d1473353feb025ea53123fc5dbe5e9
shivakishore123 <shivakishore777@gmail.com>
2024-11-28 12:24:08 +0530

    components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { AllEmployeesComponent } from './all-employees/all-employees.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'dashboard',component:DashboardComponent,children:[
+    {path:'home',component:HomeComponent},
+    {path:'create-employee',component:CreateEmployeeComponent},
+    {path:'all-employees',component:AllEmployeesComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { AllEmployeesComponent } from './all-employees/all-employees.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    CreateEmployeeComponent,
+    AllEmployeesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
